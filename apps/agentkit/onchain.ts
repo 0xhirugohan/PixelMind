65a8eb996dc0e07f34656fd34e6e244c77ceae0f
han <han@fbrns.co>
2025-02-09 18:50:07 +0700

    clean up the nft image url from moralis

--- apps/agentkit/onchain.ts
+++ apps/agentkit/onchain.ts
@@ -22,16 +22,38 @@ export async function getListNFTByAddress(
         });
 
         const response = await Moralis.EvmApi.nft.getWalletNFTs({
-            chain: "0x14a34",
+            // chain: "0x14a34", // base sepolia
+            chain: "0x1", // ethereum mainnet
             format: "hex",
-            limit: 5,
+            limit: 10,
             mediaItems: false,
             address: address,
         });
 
         const nfts: any[] = response.result;
-        const metadatas = nfts.map((nft) => nft.metadata);
-        return metadatas;
+        const images = nfts.map((nft) => {
+            const imageUri = nft?.metadata?.image || nft?.image || "";
+            return imageUri.replace(
+                "ipfs://",
+                "https://ipfs.moralis.io:2053/ipfs/",
+            );
+        });
+        const filtered: string[] = images.filter((image) => {
+            const isPng = image.indexOf(".png") != -1;
+            const isJpg = image.indexOf(".jpg") != -1;
+            const isJpeg = image.indexOf(".jpeg") != -1;
+            const isGif = image.indexOf(".gif") != -1;
+            const isWebp = image.indexOf(".webp") != -1;
+            return isPng || isJpg || isGif || isWebp;
+        });
+        const existed: string[] = [];
+        for (const image of filtered) {
+            const response = await fetch(image, {
+                method: "HEAD",
+            });
+            if (response.status === 200) existed.push(image);
+        }
+        return existed;
     } catch (err) {
         console.error("Unable to get list of NFT by address");
         console.error(err);
